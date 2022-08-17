ca17e339a4fb885e4aed72d55cbc95185c0e67f6
kenorb <kenorb@users.noreply.github.com>
2022-08-17 16:49:02 +0100

    Adds TaskRunner.cpp

---
+++ src/TaskRunner.cpp
@@ -0,0 +1,35 @@
+//+------------------------------------------------------------------+
+//|                                                     FX31337 wasm |
+//|                                 Copyright 2022-2022, EA31337 Ltd |
+//|                          https://github.com/FX31337/FX31337-wasm |
+//+------------------------------------------------------------------+
+
+/*
+ * This file is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * @file
+ * Task runner.
+ */
+
+// Includes.
+#include "classes/Common.define.h"
+#include "classes/Common.extern.h"
+#include "classes/Std.h"
+#include "classes/String.extern.h"
+#include "classes/Task/Task.h"
+
+int main(int argc, char **argv) {
+  printf("Hello World!\n");
+  return 0;
+}
